1394f1c350b28993870e0d8bdd072a929613bff2
jamesprivategmail-commits <jamesprivategmail@gmail.com>
2026-07-18 08:49:19 +0100

    Delete dice.js

--- plugins/dice.js
+++
@@ -1,8 +0,0 @@
-export default {
-    command: 'dice',
-    description: 'Roll a dice',
-    async handler(sock, msg, args, { chatId }) {
-        const roll = Math.floor(Math.random() * 6) + 1;
-        await sock.sendMessage(chatId, { text: `🎲 You rolled: ${roll}` }, { quoted: msg });
-    },
-};
