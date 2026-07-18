abab64a987a7c2106b935d0fcd212906c3d454a6
jamesprivategmail-commits <jamesprivategmail@gmail.com>
2026-07-18 09:01:20 +0100

    Delete jid.js

--- plugins/jid.js
+++
@@ -1,7 +0,0 @@
-export default {
-    command: 'jid',
-    description: 'Get this chat\'s WhatsApp ID',
-    async handler(sock, msg, args, { chatId }) {
-        await sock.sendMessage(chatId, { text: `🆔 Chat ID:\n${chatId}` }, { quoted: msg });
-    },
-};
