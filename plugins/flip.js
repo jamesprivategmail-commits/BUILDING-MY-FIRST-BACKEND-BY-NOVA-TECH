4d1177e1fe6f106c4a02d3833336c7551cec0608
jamesprivategmail-commits <jamesprivategmail@gmail.com>
2026-07-18 08:49:57 +0100

    Delete flip.js

--- plugins/flip.js
+++
@@ -1,8 +0,0 @@
-export default {
-    command: 'flip',
-    description: 'Flip a coin',
-    async handler(sock, msg, args, { chatId }) {
-        const result = Math.random() < 0.5 ? 'Heads' : 'Tails';
-        await sock.sendMessage(chatId, { text: `🪙 ${result}!` }, { quoted: msg });
-    },
-};
