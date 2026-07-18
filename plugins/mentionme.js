c7a498b2872d33f6245a93aa7ca82b3734a2f7b2
jamesprivategmail-commits <jamesprivategmail@gmail.com>
2026-07-18 08:55:24 +0100

    Delete mentionme.js

--- plugins/mentionme.js
+++
@@ -1,9 +0,0 @@
-export default {
-    command: 'mentionme',
-    description: 'Mention yourself with a message',
-    async handler(sock, msg, args, { chatId }) {
-        const senderJid = msg.key.participant || msg.key.remoteJid;
-        const text = args.join(' ') || '👋';
-        await sock.sendMessage(chatId, { text: `@${senderJid.split('@')[0]} ${text}`, mentions: [senderJid] }, { quoted: msg });
-    },
-};
